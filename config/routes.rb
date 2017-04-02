f78f9ce1835d27d1fedfa86987c3cbc4580d6d23
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-02 18:32:53 -0500

    sessions & static pages skeletons

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions_controller/new'
+
+  get 'sessions_controller/create'
+
+  get 'sessions_controller/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+	
+	get 		'/login', to: 'sessions#new'  
+	post 		'/login', to: 'sessions#create'  
+	delete 	'/logout', to: 'sessions#destroy'  
+
+  resources :users
 end
