9654f50f9878c4a8b2875034562fe53342f6685e
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-03 14:27:38 -0500

    integration test for layout/home

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout page" do 
   	get root_path
   	assert_template 'static_pages/home'
+  	assert_select "a[href=?]", root_path
+  	assert_select "a[href=?]", login_path
+  	assert_select "a[href=?]", logout_path
+  	assert_select "a[href=?]", new_user_path
+  	assert_select "section.flash-section"
   end
 end
