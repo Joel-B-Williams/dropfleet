b80dd64f17bc4e80acc8fd1b890f98d88c32ba63
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-02 19:20:22 -0500

    update user validations, flash into partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
 	has_secure_password
-	validates_confirmation_of :password
 	
+	validates :username, presence: true, length: { maximum: 50 }
+	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+	validates_confirmation_of :password
 end
