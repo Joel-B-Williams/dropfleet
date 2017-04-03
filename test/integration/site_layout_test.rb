3ddf88eb4025f7f10a6e42a746584f22fddc77e0
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-03 14:42:41 -0500

    user signup integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,4 +14,32 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   	assert_select "a[href=?]", new_user_path
   	assert_select "section.flash-section"
   end
+
+  test "user signup scenario" do 
+  	# user signs up with invalid info
+  	get root_path
+  	get new_user_path
+  	assert_template 'users/new' 
+  	assert_select 'section.form'
+  	post users_path, params: { user: { username: "",
+  												password: "password",
+  												password_confirmation: "password" } }
+  	assert_template 'users/new'
+  	post users_path, params: { user: { username: "User",
+  												password: "password",
+  												password_confirmation: "passwor" } }
+  	assert_template 'users/new'
+  	post users_path, params: { user: { username: "User",
+  												password: "pass",
+  												password_confirmation: "pass" } }
+  	assert_template 'users/new'
+  	# user signs up with valid information
+  	post users_path, params: { user: { username: "User",
+  												password: "password",
+  												password_confirmation: "password" } }
+  	assert_redirected_to root_path
+  	follow_redirect!
+  	assert_template 'static_pages/home'
+  	assert_not flash.empty?
+  end
 end
