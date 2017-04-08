59fbd0c3191588581a4786fa81e4d153ec6a8ee8
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-07 21:26:31 -0500

    Group helpers & display of group/ship stats

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,48 @@
 module GroupsHelper
+
+	def cost(group)
+		group.ship.cost * group.group_size
+	end
+
+	def ship_name(group)
+		group.ship.name 
+	end
+
+	def size(group)
+		group.group_size
+	end
+
+	def scan(group)
+		group.ship.scan
+	end
+
+	def signature(group)
+		group.ship.signature
+	end
+
+	def thrust(group)
+		group.ship.thrust
+	end
+
+	def hull(group)
+		group.ship.hull
+	end
+
+	def armor(group)
+		group.ship.armor
+	end
+
+	def point_defense(group)
+		group.ship.point_defense
+	end
+
+	def tonnage(group)
+		group.ship.tonnage.tonnage
+	end
+
+	def special_rules(group)
+		# THIS DOES NOT WORK YET NO JOIN TABLE
+		group.ship.special_rules
+	end
+
 end
