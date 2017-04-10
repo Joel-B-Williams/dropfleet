4dc30b0150659555971bfd9001462d2fad0fb307
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-10 13:19:57 -0500

    custom validation for group size

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,5 +6,21 @@ class Group < ApplicationRecord
 	has_one :faction, through: :fleet
 	has_one :user, through: :fleet
 
+
 	# validates :group_size, numericality: { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 3 }
+	# validates :group_size, numericality: { :greater_than_or_equal_to => self.ship.min_group_size, :less_than_or_equal_to => self.ship.max_group_size }
+	validate :group_size_above_min, :group_size_below_max
+
+	def group_size_above_min
+		if group_size < ship.min_group_size
+			errors.add(:group_size, "must be greater than #{ ship.min_group_size - 1 }")
+		end
+	end
+
+	def group_size_below_max
+		if group_size > ship.max_group_size
+			errors.add(:group_size, "must be less than #{ ship.max_group_size + 1 }")
+		end
+	end
+
 end
