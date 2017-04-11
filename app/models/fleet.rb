11583cd338e9cf85c65986aa330d780bbe6374c5
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-11 14:36:21 -0500

    Remove errant fleet edit, start fleet battlegroup validations

--- app/models/fleet.rb
+++ app/models/fleet.rb
@@ -9,4 +9,77 @@ class Fleet < ApplicationRecord
 	validates :name, presence: true
 	validates :points_level, presence: true
 	validates :faction, presence: true
+
+	validate 
+
+	def max_battlegroups
+		if cost < 1000
+			4
+		elsif cost < 2000
+			6
+		else 
+			7
+		end
+	end
+
+	private
+
+		def min_pathfinder
+			if cost < 1000
+				0
+			else
+				1
+			end
+		end
+		
+		def max_pathfinder	
+			if cost	< 2000	
+				2
+			else
+				3
+			end
+		end
+		
+		def min_line
+			1
+		end
+		
+		def max_line
+			if cost < 1000
+				2
+			elsif cost < 2000
+				3
+			else
+				4
+			end
+		end
+		
+		def min_vanguard
+			0
+		end
+		
+		def max_vanguard
+			if cost < 1000
+				1
+			elsif cost < 2000
+				2
+			else
+				3
+			end
+		end
+		
+		def min_flag
+			0
+		end
+		
+		def max_flag
+			if cost < 1000
+				0
+			elsif cost < 2000
+				1
+			else
+				2
+			end
+		end
+
 end
