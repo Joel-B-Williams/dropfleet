bc6d1031f9525442db60a5413412d95d819aad62
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-11 16:26:21 -0500

    show links to add BG based on max BG count by type

--- app/helpers/fleets_helper.rb
+++ app/helpers/fleets_helper.rb
@@ -36,6 +36,26 @@ module FleetsHelper
 		fleet.battlegroups.length < fleet.max_battlegroups
 	end
 
+	def can_add_flag(fleet)
+		flag_count = fleet.battlegroups.where(battlegroup_type: 1).length
+		flag_count < fleet.max_flag
+	end
+
+	def can_add_vanguard(fleet)
+		vanguard_count = fleet.battlegroups.where(battlegroup_type: 2).length
+		vanguard_count < fleet.max_vanguard
+	end
+
+	def can_add_line(fleet)
+		line_count = fleet.battlegroups.where(battlegroup_type: 3).length
+		line_count < fleet.max_line
+	end
+	
+	def can_add_pathfinder(fleet)
+		pathfinder_count = fleet.battlegroups.where(battlegroup_type: 4).length
+		pathfinder_count < fleet.max_pathfinder
+	end
+
 
 
 	# def show_battlegroups(fleet, bg_type)
