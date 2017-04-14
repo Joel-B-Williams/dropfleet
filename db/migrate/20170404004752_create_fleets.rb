a0a6081b340e194e213aa70c7db6457655adaa4e
Joel-B-Williams <joel.b.williams.is@gmail.com>
2017-04-13 23:20:26 -0500

    Schema issue faction_id on fleet

--- db/migrate/20170404004752_create_fleets.rb
+++ db/migrate/20170404004752_create_fleets.rb
@@ -2,7 +2,7 @@ class CreateFleets < ActiveRecord::Migration[5.0]
   def change
     create_table :fleets do |t|
     	t.string :name,	 				 	presence: true
-    	t.string :faction, 				presence: true
+    	t.integer :faction_id, 		presence: true
     	t.integer :points_level,	presence: true
     	t.integer :cost, 				 	presence: true, default: 0
 
